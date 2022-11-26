a68c7e859c0b508cf0f29b8887953e41a8a22918
Ryo Igarashi <n33t5hin@gmail.com>
2022-11-26 15:09:13 +0900

    fix: Fix build command

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import autoExternal from "rollup-plugin-auto-external";
-import dts from "rollup-plugin-dts";
-import typescript from "@rollup/plugin-typescript";
+const commonjs = require("@rollup/plugin-commonjs");
+const json = require("@rollup/plugin-json");
+const autoExternal = require("rollup-plugin-auto-external");
+const { default: dts } = require("rollup-plugin-dts");
+const typescript = require("@rollup/plugin-typescript");
 
-import packageJSON from "./package.json";
+const packageJSON = require("./package.json");
 
-export default [
+module.exports = [
   {
     input: "./src/index.ts",
     output: {
-      file: packageJSON.main,
+      file: packageJSON.exports["."].require,
       format: "cjs",
     },
     plugins: [json(), typescript(), autoExternal()],
@@ -18,7 +18,7 @@ export default [
   {
     input: "./src/index.ts",
     output: {
-      file: packageJSON.module,
+      file: packageJSON.exports["."].import,
       format: "esm",
     },
     plugins: [commonjs(), json(), typescript(), autoExternal()],
@@ -26,7 +26,7 @@ export default [
   {
     input: "./src/index.ts",
     output: {
-      file: packageJSON.types,
+      file: packageJSON.exports["."].types,
       format: "esm",
     },
     plugins: [dts()],
